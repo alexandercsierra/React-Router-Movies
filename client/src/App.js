a1c498a9ecdb6716280bdee5422d087253ee7e9f
Alexander Sierra <alexandercsierra@gmail.com>
2019-12-16 16:10:46 -0500

    saved movies link to their card

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ const App = () => {
     <div>
       <SavedList list={savedList} />
       <Route exact path="/"><MovieList /></Route>
-      <Route path="/movies/:id"><Movie/></Route>
+      <Route path="/movies/:id"><Movie addToSavedList = {addToSavedList}/></Route>
     </div>
   );
 };
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {useParams} from 'react-router-dom';
+import {useParams, Link} from 'react-router-dom';
 import MovieCard from './MovieCard'
 
 const Movie = (props) => {
@@ -22,18 +22,23 @@ const Movie = (props) => {
   },[id]);
   
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie)
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
+  // const { title, director, metascore, stars } = movie;
   return (
-    <MovieCard key={movie.id} movie={movie} />
+    <div>
+      <MovieCard key={movie.id} movie={movie} />
+      <button onClick={()=>{
+        saveMovie()
+        }}>Save Movie</button>
+    </div>
   );
 }
 
